309bdcb98a22b9dc808ea7eee26816d37fcb6461
liyepai <liyejiano@qq.com>
2020-01-16 20:56:35 +0800

    引入头部与底部

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,27 @@
 <template>
   <div>
+    <Herder></Herder>
+   
     <nuxt />
+     <Footer></Footer>
   </div>
 </template>
 
-<style>
+
+
+<script>
+import Herder from '@/components/header'
+import Footer from '@/components/footer'
+
+export default {
+   components:{
+     Herder,Footer
+   }
+}
+</script>
+
+
+<style lang='less'>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -16,40 +33,22 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-
-*,
-*:before,
-*:after {
+*{
+  margin:0; 
+  padding:0;
   box-sizing: border-box;
-  margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+ul, li, ol{
+  list-style:none;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+a{
+  text-decoration:none;
+  color:inherit;
 }
+a:hover{
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
 }
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,i{
+  font-style: normal;
 }
 </style>
