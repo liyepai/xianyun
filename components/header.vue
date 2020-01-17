d1bcbbd927c6a0b2f806a1938e03fe2ec28fb5a3
liyepai <liyejiano@qq.com>
2020-01-17 17:34:28 +0800

    头部完成

--- components/header.vue
+++ components/header.vue
@@ -2,20 +2,43 @@
   <div class="herder">
     <!-- 左边 -->
     <el-row type="flex" justify="space-between" align="middle" class="left">
-      <img src="logo.jpg" alt="" />
-
+      <img src="../static/logo.jpg" alt="" class="logo" />
       <el-row type="flex" class="daohan">
         <nuxt-link to="/">首页</nuxt-link>
         <nuxt-link to="./post">旅游攻略</nuxt-link>
         <nuxt-link to="./hotel">酒店</nuxt-link>
         <nuxt-link to="./air">国内机票</nuxt-link>
       </el-row>
-
       <!-- 右边 -->
       <div class="right">
-        <nuxt-link to="/user/login">登录/注册</nuxt-link>
+        <nuxt-link to="/user/login" v-if="!$store.state.user.userInfo.token"
+          >登录/注册</nuxt-link
+        >
+        <el-dropdown v-if="$store.state.user.userInfo.token">
+          <span class="el-dropdown-link">
+            <img
+              :src="
+                $axios.defaults.baseURL +
+                  $store.state.user.userInfo.user.defaultAvatar
+              "
+              alt=""
+              class="imgg"
+              width="36px"
+              height="36px"
+            />
+            {{ $store.state.user.userInfo.user.nickname }}
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人中心</el-dropdown-item>
+            <el-dropdown-item>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
       </div>
     </el-row>
+    <!-- </el-row>
+    
+  </el-row>
+    </el-row> -->
   </div>
 </template>
 
@@ -27,13 +50,13 @@ export default {};
 .herder {
   width: 1000px;
   height: 60px;
-  align-items: center;  
+  align-items: center;
   margin: 0 auto;
   box-shadow: 0 3px 0 #f5f5f5;
 }
 .left {
   display: flex;
-  img {
+  .logo {
     display: block;
     width: 156px;
     height: 42px;
